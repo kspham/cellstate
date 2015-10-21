6245b947dcd75c69ab5c048266225f0c6acfef2f
Nadiya Sitdykova <rovenskasa@gmail.com>
2015-10-21 16:29:40 -0700

    add dimension reduction

--- scripts/cellstate.py
+++ scripts/cellstate.py
@@ -1,6 +1,9 @@
 __author__ = 'nadya'
 import math
 import mst
+import numpy as np
+from sklearn import decomposition
+
 
 class Point:
     def __init__(self, sample_name, coordinates):
@@ -16,6 +19,13 @@ def binary_normalization(points, cutoff):
     for point in points:
         point.coords = [int(x > cutoff) for x in point.coords]
 
+def reduce_dimensions(points, n):
+    dataMatrix = np.array([point.coords for point in points])
+    pca = decomposition.PCA(n_components=n)
+    dataMatrix = pca.fit_transform(dataMatrix)
+    for i in range(len(points)):
+        points[i].coords = list(dataMatrix[i])
+
 def read_points(filename):
     with open(filename, 'r') as file:
         sample_names = file.readline().split()[1:]
@@ -28,20 +38,18 @@ def read_points(filename):
             line = file.readline()
     return points
 
-def cell_state(input_file, normalization, cutoff, distance_metric):
+def cell_state(input_file, normalization, cutoff, distance_metric, dims=None):
     points = read_points(input_file)
     normalization(points, cutoff)
+    if dims != None:
+        reduce_dimensions(points, dims)
     return mst.mst(points, distance_metric)
 
-
-"""mst1 = cell_state("data/data.txt", standard_normalization, None, mst.euclidean_distance)
-mst2 = cell_state("data/data.txt", binary_normalization, 500, mst.euclidean_distance)
+mst1 = cell_state("data/data.txt", standard_normalization, None, mst.euclidean_distance)
+mst2 = cell_state("data/data.txt", standard_normalization, None, mst.euclidean_distance, 70)
+#mst2 = cell_state("data/data.txt", binary_normalization, 500, mst.euclidean_distance)
 common = 0
 for edge in mst1:
     if edge in mst2:
         common += 1
-print(common)""
-
-
-
-
+print(common)
